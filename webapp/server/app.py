7cff431a85928d56eff48eb518f2b1c6fad009f1
carlos <carlos.urena@student.fairfield.edu>
2019-04-18 17:02:16 -0400

    update

--- webapp/server/app.py
+++ webapp/server/app.py
@@ -1,4 +1,23 @@
 from __future__ import division, print_function
+
+from gevent.pywsgi import WSGIServer
+from werkzeug.utils import secure_filename
+from flask import Flask, redirect, url_for, request, render_template, jsonify, json
+from keras.preprocessing import image
+from keras.models import load_model
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+import matplotlib.pyplot as plt
+import cv2
+import biosppy
+from keras.optimizers import Adam
+from keras.layers import Dense
+from keras.models import Sequential
+import keras.backend as K
+from sklearn.svm import SVR
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+
 # coding=utf-8
 import sys
 import os
@@ -8,21 +27,15 @@ import numpy as np
 import csv
 from flask_cors import CORS
 
+# Time Series
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 # Needed for ecg model
-import biosppy
-import pandas as pd
-import cv2
-import matplotlib.pyplot as plt
 
 # Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template, jsonify, json
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
@@ -40,6 +53,7 @@ MODEL_PATH = 'models/your_model.h5'
 
 @app.route('/timeforcasting/predict', methods=['POST'])
 def timeforcasingPredict():
+    print("timeforcast/print call debug enabled")
     if request.method == 'POST':
         # Get the file from post request
         f = request.files["file"]
@@ -49,18 +63,96 @@ def timeforcasingPredict():
             basepath, 'uploads/timeforcasting', secure_filename(f.filename))
         f.save(file_path)
 
-        # process input data through NN model
+        # Load file and perform basic validation
+        if not os.path.isfile(file_path):
+            print("400: File not found")
+            return jsonify(message="File not found"), 400
+        if os.path.getsize(file_path) == 0:
+            print("400: Empty file found")
+            return jsonify(message="Empty file found"), 400
 
-        # return output as JSON
-        #response = jsonify(data)
-        # return response
+        df = pd.read_csv(file_path)
+        if 'HEART_RATE' not in df.columns:
+            print("400: File missing HEART_RATE")
+            return jsonify(message="File missing HEART_RATE"), 400
+        if 'CHARTTIME' not in df.columns:
+            print("400: File missing CHARTTIME")
+            return jsonify(message="File missing CHARTTIME"), 400
 
+        heart_rate = df[['CHARTTIME', 'HEART_RATE']]
+
+        # Make the index a time datatype, make only one reading per hour and fill in missing values
+        heart_rate['CHARTTIME'] = pd.to_datetime(heart_rate['CHARTTIME'])
+        heart_rate = heart_rate.set_index('CHARTTIME')
+        heart_rate_resampled = heart_rate.resample('H').mean()
+        heart_rate_resampled = heart_rate_resampled.interpolate(
+            method='linear')
+
+        # Use last 24 hours as test data
+        raw_values = heart_rate_resampled.values
+        train, test = raw_values[0:-24], raw_values[-24:]
+        sc = MinMaxScaler()
+        train_sc = sc.fit_transform(train)
+        test_sc = sc.transform(test)
+        train_sc_df = pd.DataFrame(train_sc, columns=['Y'])
+        test_sc_df = pd.DataFrame(test_sc, columns=['Y'])
+        for s in range(1, 2):
+            train_sc_df['X_{}'.format(s)] = train_sc_df['Y'].shift(s)
+            test_sc_df['X_{}'.format(s)] = test_sc_df['Y'].shift(s)
+        X_train = train_sc_df.dropna().drop('Y', axis=1)
+        y_train = train_sc_df.dropna().drop('X_1', axis=1)
+        X_test = test_sc_df.dropna().drop('Y', axis=1)
+        y_test = test_sc_df.dropna().drop('X_1', axis=1)
+        X_train = X_train.as_matrix()
+        y_train = y_train.as_matrix()
+        X_test = X_test.as_matrix()
+        y_test = y_test.as_matrix()
+        regressor = SVR(kernel='rbf')
+        regressor.fit(X_train, y_train)
+        y_pred = regressor.predict(X_test)
+        #r2_test = mean_squared_error(y_test, y_pred)
+        K.clear_session()
+
+        # Specify two layer nueral network
+        model = Sequential()
+        model.add(Dense(50, input_shape=(
+            X_test.shape[1],), activation='relu', kernel_initializer='lecun_uniform'))
+        model.add(Dense(50, input_shape=(X_test.shape[1],), activation='relu'))
+        model.add(Dense(1))
+        model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error')
+        model.fit(X_train, y_train, batch_size=12, epochs=24, verbose=0)
+        y_pred = model.predict(X_test)
+
+        # Get data for response
+        timestamp = heart_rate_resampled.index
+        inputBPM = heart_rate['HEART_RATE'].tolist()
+        predictBPM = sc.inverse_transform(y_pred).tolist()
+
+        maxBPM = max(inputBPM)
+        minBPM = min(inputBPM)
+
+        # Round up/down to nearest 5
+        minBPM = minBPM - (minBPM % 5)
+        maxBPM = maxBPM + (maxBPM % 5)
+
+        # Pad list with nul values
+        predictBPM = [None] * (len(inputBPM)-24) + predictBPM
+
+        # TODO generate actual response
         # mock response
-        timestamp = ['1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00']
-        bpm = [78, 92, 84, 82, 90, 87, 94]
-        response = jsonify(timestamp=timestamp, bpm=bpm)
+        #timestamp = ['1:00','2:00','3:00','4:00','5:00','6:00','7:00']
+        #inputBPM = [78, 92, 84, 82, None, None, None]
+        #predictBPM = [None, None, None, None, 92, 90, 86]
+        #maxBPM = 92
+        #minBPM = 78
+
+        response = jsonify(timestamp=timestamp.strftime('%m/%d/%Y %H:%M').tolist(),
+                           inputBPM=inputBPM, predictBPM=predictBPM, min=minBPM, max=maxBPM)
+
+        print("timeforcast/print returning calculated values")
         return response
 
+    print("timeforcast/print returning nothing")
     return None
 
 
@@ -74,6 +166,20 @@ def ecgClassPredict():
         file_path = os.path.join(
             basepath, 'uploads/ecgclassification', secure_filename(f.filename))
         f.save(file_path)
+
+        # Load file and perform basic validation
+        if not os.path.isfile(file_path):
+            print("400: File not found")
+            return jsonify(message="File not found"), 400
+        if os.path.getsize(file_path) == 0:
+            print("400: Empty file found")
+            return jsonify(message="Empty file found"), 400
+
+        df = pd.read_csv(file_path)
+        if 'ECG_VALUE' not in df.columns:
+            print("400: File missing ECG_VALUE")
+            return jsonify(message="File missing ECG_VALUE"), 400
+
         print("sending this path to the model: " + file_path)
         output = runModel(file_path)
 
@@ -125,7 +231,7 @@ def model_predict(file_path, model):
     kernel = np.ones((4, 4), np.uint8)
 
     csv = pd.read_csv(file_path)
-    csv_data = csv[' Sample Value']
+    csv_data = csv['ECG_VALUE']
     data = np.array(csv_data)
     signals = []
     count = 1
